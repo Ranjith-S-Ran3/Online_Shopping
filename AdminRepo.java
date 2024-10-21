bbc12c3d91f3e92371d1db6dda9df3d51aa373b1
Ranjith-S-Ran3 <ranjith4092331@gmail.com>
2024-10-22 00:42:28 +0530

    Add files via upload

---
+++ AdminRepo.java
@@ -0,0 +1,23 @@
+package online_application;
+	
+	import java.util.*;
+public class AdminRepo {
+
+	
+	
+	private ArrayList<Admin> adminList = new ArrayList<>();
+
+	    public ArrayList<Admin> getAdminList() {
+	        return adminList;
+	    }
+	    public void setAdminList(ArrayList<Admin> adminList) {
+	        this.adminList = adminList;
+	    }
+
+	    public void viewAdmins(){
+	        for (Admin admin : adminList) {
+	            System.out.println(admin);
+	        }
+	    }
+	}
+
